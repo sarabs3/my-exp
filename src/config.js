9168682877ccfd728fd545a6d2c684569f0b07b1
Sarabjeet Singh <sarabsingh212@gmail.com>
2018-11-21 23:03:43 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 export const firebaseConfig = {
-    apiKey: "AIzaSyAGoLCar8hrofLN3wIUI41XFgOVODz_wSM",
-    authDomain: "my-exp-2e5af.firebaseapp.com",
-    databaseURL: "https://my-exp-2e5af.firebaseio.com",
-    projectId: "my-exp-2e5af",
-    storageBucket: "my-exp-2e5af.appspot.com",
-    messagingSenderId: "201538918294"
+    
 };
 
 // Initialize firebase instance
