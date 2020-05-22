263f261dbea4ce0c1fb4d0a75a003b67101b0950
Sarabjeet Singh <sarabsingh212@gmail.com>
2020-05-23 00:13:36 +0530

    Add Income form

--- src/pages/addIncome/AddIncome.jsx
+++ src/pages/addIncome/AddIncome.jsx
@@ -1,186 +1,95 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 import {
-    Layout,
-    Form,
-    Input,
-    Select,
-    Row,
-    Col,
-    Checkbox,
-    Button,
-    Divider
+  Layout,
+  Input,
+  Select,
+  Row,
+  Col,
+  DatePicker,
+  Button,
+  Divider
 } from 'antd';
 import './addIncome.css';
-import DatePickerInput, {
-    InputWrap,
-    SelectWrap
-} from "../../components/datePickerInput";
-
+import { Formik, Form, Field } from 'formik';
 
 const {Content} = Layout;
-const FormItem = Form.Item;
 const Option = Select.Option;
-const { TextArea } = Input;
 
-class AddIncome extends React.Component {
-    constructor () {
-    super()
-    this.state = {
-        startDate: moment()
-    }
-    }
-    // clear Form
-    clearForm = () => {
-        this.setState(() => ({}))
-    }
-    // handlesubmit
-    handleSubmit = (e) => {
-        if (!e.mode)
-            e.mode = 'Cash'
-        if(!e.category)
-            e.category = 'food'
-        if (!e.date)
-            e.date = moment().unix()*1000
-        if (e.mode.includes('Credit') || e.mode === 'Paytm') {
-            e.calculate = false;
-        } else {
-            e.calculate = true;
-        }
-        this.props.reset()
-        this.props.handleSubmit()
-    }
-    render() {
-        const {
-            pristine,
-            reset,
-            submitting,
-            handleSubmit,
-            paymentMode,
-            categories
-        } = this.props;
-        return (
-            <Content>
-                <Row type="flex" justify='center'>
-                    <Col span={22}>
-                        <Divider />
-                        <h1>Add Income</h1>
-                        <Divider />
-                        <Form onSubmit={handleSubmit(this.handleSubmit)}>
-                            <FormItem>
-                                <Field
-                                    name="title"
-                                    component={InputWrap}
-                                    type="text"
-                                    placeholder="Title"
-                                />
-                            </FormItem>
-                            <FormItem>
-                                <Field
-                                    name="amount"
-                                    component={InputWrap}
-                                    type="text"
-                                    placeholder="Amount"
-                                    />
-                            </FormItem>
-                            <FormItem>
-                                <Field
-                                    name='date'
-                                    component={DatePickerInput}
-                                />
-                            </FormItem>
-                            <FormItem>
-                                <Field
-                                    name="category"
-                                    component={SelectWrap}
-                                    defaultValue='food'
-                                >
-                                    <Option value=''>
-                                        -- Select Category --
-                                    </Option>
-                                    {
-                                        categories && categories.map(
-                                            item => (
-                                                <Option
-                                                    value={item.value}
-                                                    key={item.key}
-                                                >
-                                                    {item.value}
-                                                </Option>
-                                            )
-                                        )
-                                    }
-                                </Field>
-                            </FormItem>
-                            <FormItem>
-                                <Field
-                                    name="mode"
-                                    component={SelectWrap}
-                                    defaultValue='Cash'
-                                >
-                                    <Option value=''>
-                                        -- Select Payment Method --
-                                    </Option>
-                                    {
-                                        paymentMode && paymentMode.map(
-                                            item => (
-                                                <Option
-                                                    value={item.value}
-                                                    key={item.key}
-                                                >
-                                                    {item.value}
-                                                </Option>
-                                            )
-                                        )
-                                    }
-                                </Field>
-                            </FormItem>
-                            {/*
-                            <FormItem>
-                                <label>Count </label>
-                                <Field
-                                    name="calculate"
-                                    id="calculate"
-                                    component={Checkbox}
-                                    type='checkbox'
-                                />
-                            </FormItem>
-                            <FormItem>
-                                <Field name="notes" component={TextArea} />
-                            </FormItem>
-                            */}
-                            <FormItem>
-                                <Row>
-                                    <Col span={12}>
-                                        <Button
-                                            type="primary"
-                                            htmlType="submit"
-                                            disabled={pristine || submitting}
-                                            block
-                                        >
-                                            Submit
-                                        </Button>
-                                    </Col>
-                                    <Col span={12}>
-                                        <Button
-                                            onClick={reset}
-                                            block
-                                        >
-                                            Clear Values
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </FormItem>
-                        </Form>
-                    </Col>
-                </Row>
-            </Content>
-        );
-    }
+const AddIncome = ({ onSubmit, reset, paymentMode }) => {
+  const handleSubmit = (e) => {
+    onSubmit(e);
+  };
+  return (
+      <Content>
+        <Row type="flex" justify='center'>
+          <Col span={22}>
+            <Divider />
+            <h1>Add Income</h1>
+            <Divider />
+            <Formik
+                initialValues={{ title: '', amount: '', date: moment().unix()*1000 }}
+                onSubmit={handleSubmit}
+            >
+              {() => (
+                  <Form>
+                    <div style={{ marginBottom: 20 }}>
+                      <label htmlFor="title">Title</label>
+                      <Field name="title" as={Input} />
+                    </div>
+                    <div style={{ marginBottom: 20 }}>
+                      <label htmlFor="amount">Amount</label>
+                      <Field name="amount" as={Input} />
+                    </div>
+                    <div style={{ marginBottom: 20 }}>
+                      <label className='form-col-form-label'  htmlFor="date">Date</label>
+                      <DatePicker
+                          autoOk
+                          defaultValue={moment()}
+                      />
+                    </div>
+                    <div style={{ marginBottom: 20 }}>
+                      <Row>
+                        <Col span={12}>
+                          <Field
+                              name="mode"
+                              style={{ width: '100%' }}
+                              component={Select}
+                              defaultValue='Cash'
+                          >
+                            <Option value=''>
+                              -- Select Payment Method --
+                            </Option>
+                            {
+                              paymentMode && paymentMode.map(item => (
+                                      <Option value={item.value} key={item.key}>
+                                        {item.value}
+                                      </Option>
+                                  )
+                              )
+                            }
+                          </Field>
+                        </Col>
+                      </Row>
+                    </div>
+                    <div>
+                      <Row>
+                        <Col span={12}>
+                          <Button type="primary" htmlType="submit" block>Submit</Button>
+                        </Col>
+                        <Col span={12}>
+                          <Button onClick={reset} block >Clear Values</Button>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Form>
+              )}
+            </Formik>
+          </Col>
+        </Row>
+      </Content>
+  );
 };
 
-export default reduxForm({
-    form: 'addIncome',
-})(AddIncome)
+export default AddIncome;
